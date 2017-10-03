fa50401c912c10bea0c9c95697c638793e7cb911
vasterlord <vasterlord619@gmail.com>
2017-10-03 19:46:15 +0300

    Allure report config good test

--- src/test/java/com/epam/lab/gmailframework/GmailTest.java
+++ src/test/java/com/epam/lab/gmailframework/GmailTest.java
@@ -6,47 +6,50 @@ import com.epam.lab.gmailframework.models.User;
 import com.epam.lab.gmailframework.pageobjects.AllureLogger;
 import com.epam.lab.gmailframework.utils.DataUtils;
 import com.epam.lab.gmailframework.utils.WebDriverUtils;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Title;
 
 import java.util.List;
 
+@Title("Test title")
 public class GmailTest {
 
-//    @Step(value = "tear down aft meth")
-//    @AfterMethod
-//    public void tearDown() throws Exception {
-//        WebDriverUtils.quit();
-//    }
-//
-//    @Step(value = "gmail test")
-//    @Test(dataProvider = "testData", threadPoolSize = 3)
-//    public void testGmailFunctionality(User user) {
-//        AllureLogger.logToAllure("Start test");
-//        GmailLoginBO gmailLoginBO = new GmailLoginBO();
-//        gmailLoginBO.signIn(user);
-//        GmailHomeBO gmailHomeBO = new GmailHomeBO();
-//        Assert.assertTrue(gmailHomeBO.isSignIn());
-//        Assert.assertTrue(gmailHomeBO.isLetterSavedInDraft(user.getLetter()));
-//        Assert.assertTrue(gmailHomeBO.isLetterSent(user.getLetter()));
-//    }
-//
-//    @Step(value = "dataprov")
-//    @DataProvider(name = "testData", parallel = true)
-//    public static Object[][] getTestData() throws Exception {
-//        List<User> userList = DataUtils.getUsersDataFromXML().getUsers();
-//        int rowCount = userList.size();
-//        int columnCount = 1;
-//        Object[][] testData = new Object[rowCount][columnCount];
-//        for (int i = 0; i < rowCount; i++) {
-//            testData[i][0] = userList.get(i);
-//        }
-//        return testData;
-//    }
+    @Title("Test tear")
+    @Step(value = "tear down aft meth")
+    @AfterMethod
+    public void tearDown() throws Exception {
+        WebDriverUtils.quit();
+    }
+
+    @Title("Test")
+    @Step(value = "gmail test")
+    @Test(dataProvider = "testData", threadPoolSize = 3)
+    public void testGmailFunctionality(User user) {
+        AllureLogger.logToAllure("Start test");
+        GmailLoginBO gmailLoginBO = new GmailLoginBO();
+        gmailLoginBO.signIn(user);
+        GmailHomeBO gmailHomeBO = new GmailHomeBO();
+        Assert.assertTrue(gmailHomeBO.isSignIn());
+        Assert.assertTrue(gmailHomeBO.isLetterSavedInDraft(user.getLetter()));
+        Assert.assertTrue(gmailHomeBO.isLetterSent(user.getLetter()));
+    }
+
+    @Title("Test data")
+    @Step(value = "dataprov1")
+    @DataProvider(name = "testData", parallel = true)
+    public static Object[][] getTestData() throws Exception {
+        List<User> userList = DataUtils.getUsersDataFromXML().getUsers();
+        int rowCount = userList.size();
+        int columnCount = 1;
+        Object[][] testData = new Object[rowCount][columnCount];
+        for (int i = 0; i < rowCount; i++) {
+            testData[i][0] = userList.get(i);
+        }
+        return testData;
+    }
 
 }
