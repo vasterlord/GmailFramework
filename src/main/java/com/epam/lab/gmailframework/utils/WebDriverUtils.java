3cb5a3dce730758e8371c763051bbf019e87aacb
vasterlord <vasterlord619@gmail.com>
2017-10-09 18:52:40 +0300

    Working version with notify thread features (refactored)

--- src/main/java/com/epam/lab/gmailframework/utils/WebDriverUtils.java
+++ src/main/java/com/epam/lab/gmailframework/utils/WebDriverUtils.java
@@ -10,39 +10,39 @@ public class WebDriverUtils {
     private static int counter = 0;
     private static final int THREADS_COUNT_IN_ONE_TIME = 3;
 
-    public static WebDriver getWebDriverThreadLocal() {
-        ConfigurationProperties configurationProperties = new ConfigurationProperties();
-        if (WEB_DRIVER_THREAD_LOCAL.get() != null) {
-            return WEB_DRIVER_THREAD_LOCAL.get();
-        }
-        System.setProperty(configurationProperties.getWebDriverName(), configurationProperties.getWebDriverPath());
-        synchronized (WEB_DRIVER_THREAD_LOCAL) {
-            while (counter == THREADS_COUNT_IN_ONE_TIME) {
-                WEB_DRIVER_THREAD_LOCAL.notify();
-            }
-            WebDriver webDriverInstance = new ChromeDriver();
-            WEB_DRIVER_THREAD_LOCAL.set(webDriverInstance);
-            counter++;
-            //WEB_DRIVER_THREAD_LOCAL.get().manage().window().maximize();
-            WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().setScriptTimeout(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
-            WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().pageLoadTimeout(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
-            WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().implicitlyWait(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
-            return WEB_DRIVER_THREAD_LOCAL.get();
-        }
-    }
-
 //    public static WebDriver getWebDriverThreadLocal() {
 //        ConfigurationProperties configurationProperties = new ConfigurationProperties();
 //        if (WEB_DRIVER_THREAD_LOCAL.get() != null) {
 //            return WEB_DRIVER_THREAD_LOCAL.get();
 //        }
 //        System.setProperty(configurationProperties.getWebDriverName(), configurationProperties.getWebDriverPath());
-//        WebDriver webDriverInstance = new ChromeDriver();
-//        WEB_DRIVER_THREAD_LOCAL.set(webDriverInstance);
-//        //WEB_DRIVER_THREAD_LOCAL.get().manage().window().maximize();
-//        WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().implicitlyWait(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
-//        return WEB_DRIVER_THREAD_LOCAL.get();
+//        synchronized (WEB_DRIVER_THREAD_LOCAL) {
+//            while (counter == THREADS_COUNT_IN_ONE_TIME) {
+//                WEB_DRIVER_THREAD_LOCAL.notify();
+//            }
+//            WebDriver webDriverInstance = new ChromeDriver();
+//            WEB_DRIVER_THREAD_LOCAL.set(webDriverInstance);
+//            counter++;
+//            //WEB_DRIVER_THREAD_LOCAL.get().manage().window().maximize();
+//            WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().setScriptTimeout(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
+//            WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().pageLoadTimeout(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
+//            WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().implicitlyWait(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
+//            return WEB_DRIVER_THREAD_LOCAL.get();
+//        }
 //    }
+
+    public static WebDriver getWebDriverThreadLocal() {
+        ConfigurationProperties configurationProperties = new ConfigurationProperties();
+        if (WEB_DRIVER_THREAD_LOCAL.get() != null) {
+            return WEB_DRIVER_THREAD_LOCAL.get();
+        }
+        System.setProperty(configurationProperties.getWebDriverName(), configurationProperties.getWebDriverPath());
+        WebDriver webDriverInstance = new ChromeDriver();
+        WEB_DRIVER_THREAD_LOCAL.set(webDriverInstance);
+        //WEB_DRIVER_THREAD_LOCAL.get().manage().window().maximize();
+        WEB_DRIVER_THREAD_LOCAL.get().manage().timeouts().implicitlyWait(configurationProperties.getTimeOutInSeconds(), TimeUnit.SECONDS);
+        return WEB_DRIVER_THREAD_LOCAL.get();
+    }
 
     private WebDriverUtils() {
     }
